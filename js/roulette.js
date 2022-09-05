83eb05c34eb0dda7050e9c260e021b1029e65813
Aleksandr-Pod <stdfire@gmail.com>
2022-09-05 14:21:17 +0300

    commit

--- js/roulette.js
+++ js/roulette.js
@@ -1,9 +1,9 @@
 import global from "./globalData.js";
 import { modalRef } from "./refs.js";
 
-export const roulette = async () => {
+export const roulette = () => {
     for (let i = 1; i <= 15; i += 1 ){
-        await setTimeout(() => {
+        setTimeout(() => {
             const result = Math.floor(Math.random()*37);
             const colorResult = ( result & 1 ) ? "red" : "black";
             modalRef.result.innerHTML = `<p class="result-number ${colorResult}">${result}</p>`;
